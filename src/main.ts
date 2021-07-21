88698ab919635cd71679ef70f902721a9c0f005d
Marcel Kollovieh <marcel.kollovieh@tum.de>
2021-07-21 11:36:23 +0200

    Changed style of navbar + bugfix

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,5 @@
-import * as Tone from 'tone'
 import { throttle } from 'lodash'
-const button = () => {
-    document.getElementById('hd12').textContent='Gedrueckt'
-    const synth = new Tone.Synth().toDestination();
-    
-    const now = Tone.now();
-    synth.triggerAttackRelease("C4", "8n", now);
-    synth.triggerAttackRelease("E4", "8n", now + 0.5);
-    synth.triggerAttackRelease("C4", "8n", now + 0.1);
-    synth.triggerAttackRelease("E4", "8n", now + 0.21);
-    synth.triggerAttackRelease("G4", "8n", now + 1);
-};
+
 
 const sq = document.getElementById('sq'); //as HTMLDivElement
 const content = document.getElementById('content'); //as HTMLDivElement
@@ -27,7 +16,7 @@ const drawSquare = () => {
 const scrollCallback = () => {
     let relative = 1- 2* parallax.scrollTop/parallax.scrollHeight
     content.style.opacity=String(relative);
-    document.getElementById("navbar").style.top = String(relative*50)+'px';
+    document.getElementById("navbar").style.top = String(-relative*50)+'px';
     parallax.style.paddingTop = String((1-relative)*50)+'px'
     
   };
