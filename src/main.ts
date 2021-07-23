f1ea29caad1d0cce94021746cb943be31366f142
Marcel Kollovieh <marcel.kollovieh@tum.de>
2021-07-23 20:44:24 +0200

    Added contact and profile picture

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { throttle } from 'lodash'
 
-
-const sq = document.getElementById('sq'); //as HTMLDivElement
-const content = document.getElementById('content'); //as HTMLDivElement
-const navbar = document.getElementById('navbar'); //as HTMLDivElement
+const content = document.getElementById('content'); 
+const navbar = document.getElementById('navbar'); 
 let parallax = document.querySelector('.parallax') as HTMLElement
+
 const scrollToAbout = () => {
     document.getElementById("about").scrollIntoView({behavior: "smooth"});
+}
+const scrollToContacts = () => {
+    document.getElementById("contacts").scrollIntoView({behavior: "smooth"});
+}
+const scrollToContent = () => {
+    parallax.scrollTo({top: 0,
+        left: 0,
+        behavior: 'smooth'})
 }
 const scrollCallback = () => {
     let relative = 1- 2* parallax.scrollTop/parallax.scrollHeight
@@ -14,9 +21,7 @@ const scrollCallback = () => {
     //document.getElementById("navbar").style.top = String(-relative*60)+'px';
     //parallax.style.paddingTop = String((1-relative)*60)+'px'
     
-  };
-parallax.addEventListener('scroll', throttle(scrollCallback, 50));
-document.getElementById('aboutbutton').addEventListener('click', scrollToAbout);
+  }; 
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -27,12 +32,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-const scrollToContent = () => {
-    parallax.scrollTo({top: 0,
-        left: 0,
-        behavior: 'smooth'})
 
-}
 
 document.getElementById('homebutton').addEventListener("click", scrollToContent)
-document.getElementById('aboutbutton2').addEventListener("click", scrollToAbout)
+document.getElementById('aboutbutton').addEventListener('click', scrollToAbout);
+document.getElementById('aboutbutton2').addEventListener("click", scrollToAbout);
+document.getElementById('contactbutton').addEventListener('click', scrollToContacts);
+parallax.addEventListener('scroll', throttle(scrollCallback, 50));
